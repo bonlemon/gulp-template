0876fb0d650f8fac5282dd59f17b957c74773897
AvilovDI <avilov.d.i@yandex.ru>
2018-07-28 21:01:45 +0300

    add gulp task for sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,18 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('app/scss/**/*.scss')
             .pipe(sass())
-            .pipe(autoprefixer({
-                browsers: ['last 2 vertions']
-            }))
+            .pipe(autoprefixer())
             .pipe(cleanCss())
-            .pipe(gulp.dest('./css'))
+            .pipe(gulp.dest('app/css'))
 })
+
+
+gulp.task('watch', function() {
+    // gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	// gulp.watch('app/*.html', browserSync.reload);
+	// gulp.watch('app/js/**/*.js', browserSync.reload);
+
+});
